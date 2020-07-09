c9203f100d9b574d220576342e4e667caacd9669
Alexandre Araújo <a1lexandre.na@gmail.com>
2020-07-08 21:57:22 -0300

    Configuração de mais campos componetizados e suas aplicações no form

--- src/app/shared/components/campos/input-date/input-date.component.ts
+++ src/app/shared/components/campos/input-date/input-date.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+
+import { ValidarCamposService } from './../validar-campos.service';
+
 
 @Component({
-  selector: 'input-date',
+  selector: 'dio-input-date',
   templateUrl: './input-date.component.html',
   styleUrls: ['./input-date.component.scss']
 })
 export class InputDateComponent implements OnInit {
 
-  constructor() { }
+  @Input() titulo: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+
+  constructor(public validacao: ValidarCamposService) { }
 
   ngOnInit() {
+  }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
--- src/app/shared/components/campos/input-number/input-number.component.ts
+++ src/app/shared/components/campos/input-number/input-number.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ValidarCamposService } from './../validar-campos.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'input-number',
+  selector: 'dio-input-number',
   templateUrl: './input-number.component.html',
   styleUrls: ['./input-number.component.scss']
 })
 export class InputNumberComponent implements OnInit {
 
-  constructor() { }
+  @Input() titulo: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+  @Input() minimo = 0;
+  @Input() maximo = 10;
+  @Input() step = 1;
+
+  constructor(public validacao: ValidarCamposService) { }
 
   ngOnInit() {
+  }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
   }
 
 }
