c4818a6ca12188a3fbfd4d0f2f7192c5dc336bc0
Alexandre Araújo <a1lexandre.na@gmail.com>
2020-07-12 01:13:22 -0300

    Campos de pesquisa e filtragem da lista de filmes

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { FilmeService } from './../../core/filme.service';
 import { Filme } from 'src/app/shared/models/filme';
@@ -13,10 +14,20 @@ export class ListagemFilmesComponent implements OnInit {
   readonly limite = 4;
   pagina = 0;
   filmes: Filme[] = [];
+  filtragem: FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmeService: FilmeService) { }
+  constructor(private fb: FormBuilder,
+              private filmeService: FilmeService) { }
 
   ngOnInit(): void {
+    this.filtragem = this.fb.group({
+      nomeFilme: [''],
+      genero: ['']
+    });
+
+    this.generos = ['Ação', 'Aventura', 'Ficção Científica', 'Romance', 'Terror', 'Drama', 'Biografia', 'História'];
+
     this.listarFilmes();
   }
 
